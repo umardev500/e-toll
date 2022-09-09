5f7c8a484260d5dd32262ad463d8e358a2a546d7
umar <umar.dev500@gmail.com>
2022-09-10 04:29:20 +0700

    add input button

--- resources/js/pages/admin/product/Product.tsx
+++ resources/js/pages/admin/product/Product.tsx
@@ -64,14 +64,21 @@ export const Products: React.FC = () => {
                     <h1 className="text-2xl font-bold text-gray-500 mb-6 mt-2 roboto">Order Listing</h1>
 
                     <div className="flex flex-col lg:flex-row lg:justify-between lg:items-center mb-5">
-                        <button
-                            onClick={() => {
-                                setShowFilter(true)
-                            }}
-                            className="outline-none bg-blue-500 hover:bg-blue-600 text-white px-4 py-1.5 rounded-md font-medium roboto whitespace-nowrap mb-4 lg:mb-0 flex items-center justify-center lg:justify-start"
-                        >
-                            Filter viewing
-                        </button>
+                        <div className="flex gap-2">
+                            <button className="outline-none bg-yellow-500 hover:bg-blue-600 text-white px-2 py-1.5 rounded-md font-medium roboto whitespace-nowrap mb-4 lg:mb-0 flex items-center justify-center lg:justify-start">
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M6 12H18M12 18V6" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round" />
+                                </svg>
+                            </button>
+                            <button
+                                onClick={() => {
+                                    setShowFilter(true)
+                                }}
+                                className="outline-none bg-green-500 hover:bg-green-600 text-white px-4 py-1.5 rounded-md font-medium roboto whitespace-nowrap mb-4 lg:mb-0 flex items-center justify-center lg:justify-start"
+                            >
+                                Filter viewing
+                            </button>
+                        </div>
                         <Search callback={searchCallback} title="Enter the keyword" placeholder="Enter key to search" />
                     </div>
                     <div className="bg-white overflow-auto rounded-lg border mb-5">
