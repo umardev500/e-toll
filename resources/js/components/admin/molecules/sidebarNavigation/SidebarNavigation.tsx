d7cd6f5dfe66e5170e25d83b428a7daa93c60f3d
umar <umar.dev500@gmail.com>
2022-09-10 02:11:19 +0700

    use Link component

--- resources/js/components/admin/molecules/sidebarNavigation/SidebarNavigation.tsx
+++ resources/js/components/admin/molecules/sidebarNavigation/SidebarNavigation.tsx
@@ -1,32 +1,42 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SidebarElement = () => {
     return (
         <div className="px-2 mt-4">
             <ul className="sidebar-navigation">
                 <li className="overflow-hidden">
-                    <a className={`sidebar-link active rounded flex px-4 items-center hover:bg-gray-100 my-1 h-11 text-gray-500 hover:text-gray-600 font-medium`} href="/">
+                    <Link
+                        to={'/admin'}
+                        className={`outline-none sidebar-link active rounded flex px-4 items-center hover:bg-gray-100 my-1 h-11 text-gray-500 hover:text-gray-600 font-medium`}
+                    >
                         <div className="flex gap-2.5 items-center flex-1 roboto">
                             <span className="icon" style={{ ['--off' as string]: "url('/app-icon/dashboard_icon_solid.svg')" }}></span>
                             <span>Dashboard</span>
                         </div>
-                    </a>
+                    </Link>
                 </li>
                 <li className="overflow-hidden">
-                    <a className={`sidebar-link rounded flex px-4 items-center hover:bg-gray-100 my-1 h-11 text-gray-500 hover:text-gray-600 font-medium`} href="/">
+                    <Link
+                        to={'/admin/orders'}
+                        className={`outline-none sidebar-link rounded flex px-4 items-center hover:bg-gray-100 my-1 h-11 text-gray-500 hover:text-gray-600 font-medium`}
+                    >
                         <div className="flex gap-2.5 items-center flex-1 roboto">
                             <span className="icon" style={{ ['--off' as string]: "url('/app-icon/receipt.svg')" }}></span>
                             <span>Orders List</span>
                         </div>
-                    </a>
+                    </Link>
                 </li>
                 <li className="overflow-hidden">
-                    <a className={`sidebar-link rounded flex px-4 items-center hover:bg-gray-100 my-1 h-11 text-gray-500 hover:text-gray-600 font-medium`} href="/">
+                    <Link
+                        to={'/admin/settings'}
+                        className={`outline-none sidebar-link rounded flex px-4 items-center hover:bg-gray-100 my-1 h-11 text-gray-500 hover:text-gray-600 font-medium`}
+                    >
                         <div className="flex gap-2.5 items-center flex-1 roboto">
                             <span className="icon" style={{ ['--off' as string]: "url('/app-icon/settings.svg')" }}></span>
                             <span>Settings</span>
                         </div>
-                    </a>
+                    </Link>
                 </li>
             </ul>
         </div>
