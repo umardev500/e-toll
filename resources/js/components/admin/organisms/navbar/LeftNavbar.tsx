01eda1bbb9ce43daf5dd4e2c5868c70bda62dc2b
umar <umar.dev500@gmail.com>
2023-03-02 19:59:33 +0700

    refactor navbar

--- resources/js/components/admin/organisms/navbar/LeftNavbar.tsx
+++ resources/js/components/admin/organisms/navbar/LeftNavbar.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const LeftNavbar: React.FC = () => {
     return (
         <ul className="navigation flex">
             <li>
-                <a href={'/admin'} className="navigation-link px-4 roboto text-gray-500 hover:text-gray-400 font-medium">
+                <Link to={'/admin'} className="navigation-link px-4 roboto text-gray-500 hover:text-gray-400 font-medium">
                     Home
-                </a>
+                </Link>
             </li>
         </ul>
     )
--- resources/js/components/admin/organisms/navbar/RightNavbar.tsx
+++ resources/js/components/admin/organisms/navbar/RightNavbar.tsx
@@ -4,7 +4,7 @@ export const RightNavbar: React.FC = () => {
     return (
         <ul className="navigation flex">
             <li className="flex relative">
-                <a className="navigation-link avatar-item flex gap-2 items-center px-4 roboto text-gray-500 font-medium" href="#">
+                <a className="cursor-default navigation-link avatar-item flex gap-2 items-center px-4 roboto text-gray-500 font-medium" href="#">
                     <span>Jhonson</span>
                     <img src="/avatars/avatar-2.png" className="object-cover w-9 h-9 object-top rounded-full" alt="avatar" />
                 </a>
