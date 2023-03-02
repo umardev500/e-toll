a1f194f106d0b11d1c01c08a154f15b79baff477
umar <umar.dev500@gmail.com>
2023-03-02 20:39:05 +0700

    make featured list responsive

--- resources/js/components/admin/organisms/featuredList/FeaturedList.tsx
+++ resources/js/components/admin/organisms/featuredList/FeaturedList.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 export const FeaturedList: React.FC<Props> = ({ orders, newOrder, pending, confirmation }) => {
     return (
-        <div className="grid grid-cols-4 pt-5">
+        <div className={`grid lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 pt-5`}>
             <Featured icon="url('/app-icon/book-filled.svg')" color="#94a3b8" title="Orders" subTitle={orders} />
             <Featured icon="url('/app-icon/task.svg')" color="#84cc16" title="New Orders" subTitle={newOrder} />
             <Featured icon="url('/app-icon/receipt-filled.svg')" color="#eab308" title="Orders Pending" subTitle={pending} />
