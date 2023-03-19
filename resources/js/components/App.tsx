2bc980dac2bd6ba5782d1fe783021ffcd2e9a9a5
umar <umar.dev500@gmail.com>
2023-03-19 19:08:18 +0700

    show hide content on no data

--- resources/js/components/App.tsx
+++ resources/js/components/App.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext, type AppContextType } from '../context/AppContext'
 import { Checkout, SerachToll, TollList } from './organisms'
 
 export const App: React.FC = () => {
+    const context = useContext(AppContext) as AppContextType
+
     return (
         <div className="container py-10 mx-auto flex justify-center">
             <div className="px-4 flex flex-col items-center w-full">
@@ -9,15 +12,17 @@ export const App: React.FC = () => {
                 <SerachToll />
 
                 {/* Result */}
-                <div className="mt-10 p-4 w-full md:w-2/3">
-                    <div className="text-gray-600 text-lg">Nominal</div>
+                {context.products.length > 0 ? (
+                    <div className="mt-10 p-4 w-full md:w-2/3">
+                        <div className="text-gray-600 text-lg">Nominal</div>
 
-                    {/* Toll list */}
-                    <TollList />
+                        {/* Toll list */}
+                        <TollList />
 
-                    {/* Checkout */}
-                    <Checkout />
-                </div>
+                        {/* Checkout */}
+                        <Checkout />
+                    </div>
+                ) : null}
             </div>
         </div>
     )
