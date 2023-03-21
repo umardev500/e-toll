2cda036be8dd2f79a91f3c054948bb92c4f2d56a
umar <umar.dev500@gmail.com>
2023-03-21 15:36:18 +0700

    add key

--- resources/js/components/organisms/orderList/OrderList.tsx
+++ resources/js/components/organisms/orderList/OrderList.tsx
@@ -5,8 +5,8 @@ export const OrderList: React.FC = () => {
     return (
         <div className="py-4 w-full lg:w-1/2 xl:w-1/2 md:w-2/3 relative flex items-center flex-wrap">
             <div className="grid grid-cols-1 gap-7 flex-1">
-                {[...Array(5)].map(() => (
-                    <OrderListing />
+                {[...Array(5)].map((_, i) => (
+                    <OrderListing key={i} />
                 ))}
             </div>
         </div>
