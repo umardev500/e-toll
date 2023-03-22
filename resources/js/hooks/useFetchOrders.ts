73d07a4f6710c99d11362a62a46fd07fecf1d72a
umar <umar.dev500@gmail.com>
2023-03-22 15:24:13 +0700

    add fetch orders hooks

---
+++ resources/js/hooks/useFetchOrders.ts
@@ -0,0 +1,32 @@
+import { useContext, useEffect } from 'react'
+import { AppContext, type AppContextType } from '../context/AppContext'
+import { type OrderResponse } from '../types'
+
+export const useFetchOrders = () => {
+    const baseURL = import.meta.env.VITE_API_URL
+
+    const context = useContext(AppContext) as AppContextType
+
+    useEffect(() => {
+        const fetchOrders = async (): Promise<OrderResponse> => {
+            const target = `${baseURL}/orders`
+
+            try {
+                const response = await fetch(target)
+                const jsonData: OrderResponse = await response.json()
+                return jsonData
+            } catch (err) {
+                return await Promise.reject(err)
+            }
+        }
+
+        fetchOrders()
+            .then((res) => {
+                const orders = res.data
+                context.setOrders(orders)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, [])
+}
