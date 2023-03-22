4e8df15ed6302ae57dd676e656b556ead8691161
umar <umar.dev500@gmail.com>
2023-03-22 15:01:31 +0700

    add order detail

--- resources/js/components/molecules/orderListing/OrderListing.tsx
+++ resources/js/components/molecules/orderListing/OrderListing.tsx
@@ -1,58 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { OrderDetail } from '../../organisms'
+import '../../../../css/modal.css'
 
 export const OrderListing: React.FC = () => {
+    const [detailOpen, setDetailOpen] = useState<boolean>(false)
+
     return (
-        <div className="flex-1 overflow-hidden p-5 border border-gray-100 rounded-lg shadow-[0_3px_5px_rgba(0,0,0,.05)]">
-            {/* Top */}
-            <div className="flex flex-wrap items-center justify-between">
-                <div className="roboto flex items-center gap-2 whitespace-nowrap">
-                    <span className="text-gray-500 font-semibold">Pulsa</span>
-                    <span className="text-sm text-gray-500">Mar, 25 2023</span>
+        <>
+            <div className="flex-1 overflow-hidden p-5 border border-gray-100 rounded-lg shadow-[0_3px_5px_rgba(0,0,0,.05)]">
+                {/* Top */}
+                <div className="flex flex-wrap items-center justify-between">
+                    <div className="roboto flex items-center gap-2 whitespace-nowrap">
+                        <span className="text-gray-500 font-semibold">Pulsa</span>
+                        <span className="text-sm text-gray-500">Mar, 25 2023</span>
+                    </div>
+                    <div className="roboto whitespace-nowrap flex">
+                        <span className="text-gray-500 text-sm">Pay before:</span>
+                        <span className="text-orange-500 text-sm ml-2 font-medium">Mar, 25 10:15</span>
+                    </div>
                 </div>
-                <div className="roboto whitespace-nowrap flex">
-                    <span className="text-gray-500 text-sm">Pay before:</span>
-                    <span className="text-orange-500 text-sm ml-2 font-medium">Mar, 25 10:15</span>
+                {/* Center */}
+                <div className="mt-4 flex flex-col xl:flex-row gap-4 xl:gap-4 justify-between">
+                    <div className="flex items-center gap-4">
+                        <img className="w-14 hidden xl:flex" src="bca.png" alt="" />
+                        <div>
+                            <div className="roboto text-sm text-gray-500">Payment Method</div>
+                            <div className="roboto mt-1 text-gray-500 font-semibold">BCA Virtual Account</div>
+                        </div>
+                    </div>
+                    <div className="flex flex-1 xl:gap-4 items-center">
+                        <div className="w-1 h-8 border-l hidden xl:flex"></div>
+                        <div>
+                            <div className="roboto text-sm text-gray-500">Virtual Account Number</div>
+                            <div className="roboto mt-1 text-gray-500 font-semibold">80777083142765573</div>
+                        </div>
+                    </div>
+                    <div className="flex items-center xl:gap-4">
+                        <div className="w-1 h-8 border-l hidden xl:flex"></div>
+                        <div>
+                            <div className="roboto text-sm text-gray-500">Payment Total</div>
+                            <div className="roboto mt-1 text-gray-500 font-semibold">Rp25.000</div>
+                        </div>
+                    </div>
+                </div>
+                {/* Bottom */}
+                <div className="mt-8 border-gray-100">
+                    <div className="flex flex-col xl:flex-row justify-center xl:justify-between xl:items-center gap-2.5">
+                        <div className="flex items-center gap-1.5 roboto">
+                            <span className="text-gray-500 text-sm font-medium">Status:</span>
+                            <span className="text-teal-600 text-sm font-medium">Pending</span>
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <button className="w-full outline-none text-sm font-medium roboto border border-gray-300 px-4 py-1.5 rounded text-gray-400 hover:bg-gray-50">
+                                Cancel
+                            </button>
+                            <button
+                                onClick={() => {
+                                    setDetailOpen(true)
+                                }}
+                                className="w-full whitespace-nowrap outline-none text-sm font-medium roboto border border-teal-600 bg-teal-600 hover:bg-teal-700 text-white px-4 py-1.5 rounded"
+                            >
+                                See more
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
-            {/* Center */}
-            <div className="mt-4 flex flex-col xl:flex-row gap-4 xl:gap-4 justify-between">
-                <div className="flex items-center gap-4">
-                    <img className="w-14 hidden xl:flex" src="bca.png" alt="" />
-                    <div>
-                        <div className="roboto text-sm text-gray-500">Payment Method</div>
-                        <div className="roboto mt-1 text-gray-500 font-semibold">BCA Virtual Account</div>
-                    </div>
-                </div>
-                <div className="flex flex-1 xl:gap-4 items-center">
-                    <div className="w-1 h-8 border-l hidden xl:flex"></div>
-                    <div>
-                        <div className="roboto text-sm text-gray-500">Virtual Account Number</div>
-                        <div className="roboto mt-1 text-gray-500 font-semibold">80777083142765573</div>
-                    </div>
-                </div>
-                <div className="flex items-center xl:gap-4">
-                    <div className="w-1 h-8 border-l hidden xl:flex"></div>
-                    <div>
-                        <div className="roboto text-sm text-gray-500">Payment Total</div>
-                        <div className="roboto mt-1 text-gray-500 font-semibold">Rp25.000</div>
-                    </div>
-                </div>
-            </div>
-            {/* Bottom */}
-            <div className="mt-8 border-gray-100">
-                <div className="flex flex-col xl:flex-row justify-center xl:justify-between xl:items-center gap-2.5">
-                    <div className="flex items-center gap-1.5 roboto">
-                        <span className="text-gray-500 text-sm font-medium">Status:</span>
-                        <span className="text-teal-600 text-sm font-medium">Pending</span>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <button className="w-full outline-none text-sm font-medium roboto border border-gray-300 px-4 py-1.5 rounded text-gray-400 hover:bg-gray-50">Cancel</button>
-                        <button className="w-full whitespace-nowrap outline-none text-sm font-medium roboto border border-teal-600 bg-teal-600 hover:bg-teal-700 text-white px-4 py-1.5 rounded">
-                            See more
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
+
+            {detailOpen ? <OrderDetail setState={setDetailOpen} /> : null}
+        </>
     )
 }
---
+++ resources/js/components/organisms/orderDetail/OrderDetail.tsx
@@ -0,0 +1,60 @@
+import React, { useCallback, useRef } from 'react'
+import { useClickOutside } from '../../../hooks'
+
+interface Props {
+    setState: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export const OrderDetail: React.FC<Props> = ({ setState }) => {
+    const overlayRef = useRef<HTMLDivElement>(null)
+    const innerRef = useRef<HTMLDivElement>(null)
+
+    const handleClose = useCallback(() => {
+        setState(false)
+    }, [])
+
+    useClickOutside(overlayRef, innerRef, setState)
+
+    return (
+        <div ref={overlayRef} className="modal pt-5 px-5">
+            <div ref={innerRef} style={{ width: 400 }} className="modal-inner bg-white rounded-lg">
+                {/* Header */}
+                <div className="flex justify-between p-4 px-6">
+                    <h3 className="text-gray-500 font-medium roboto">Order Detail</h3>
+                    <button onClick={handleClose} className="hover:text-gray-500 text-gray-400">
+                        <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                fillRule="evenodd"
+                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                clipRule="evenodd"
+                            ></path>
+                        </svg>
+                    </button>
+                </div>
+                {/* body */}
+                <div className="px-6 pb-5 pt-4 roboto flex flex-col gap-2.5">
+                    <div className="flex items-center justify-between pb-2.5 border-b border-dashed">
+                        <span className="text-sm font-medium text-gray-500">Payment Method:</span>
+                        <span className="ml-2 text-sm text-gray-400">BCA Virtual Account</span>
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <span className="text-sm font-medium text-gray-500">Order ID:</span>
+                        <span className="ml-2 text-sm text-gray-400">166789283474</span>
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <span className="text-sm font-medium text-gray-500">Phone Number:</span>
+                        <span className="ml-2 text-sm text-gray-400">0838-7915-4310</span>
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <span className="text-sm font-medium text-gray-500">Status:</span>
+                        <span className="ml-2 text-sm text-gray-400">Pending</span>
+                    </div>
+                    <div className="flex items-center justify-between pt-2.5 border-t border-dashed">
+                        <span className="font-semibold text-gray-500">Total:</span>
+                        <span className="ml-2 font-semibold text-gray-500">Rp25.000</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
