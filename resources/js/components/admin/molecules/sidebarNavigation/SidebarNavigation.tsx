b5f98f46f171d3a05d03204c7967a44d579ad18f
umar <umar.dev500@gmail.com>
2023-03-23 16:19:37 +0700

    add navigation sidebar

---
+++ resources/js/components/admin/molecules/sidebarNavigation/SidebarNavigation.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../../../../../css/sidebar-navigation.css'
+
+const SidebarElement = () => {
+    return (
+        <div className="px-2 mt-4">
+            <ul className="sidebar-navigation">
+                <li className="overflow-hidden">
+                    <a className={`sidebar-link active rounded flex px-4 items-center hover:bg-gray-100 my-1 h-11 text-gray-500 hover:text-gray-600 font-medium`} href="/">
+                        <div className="flex gap-2.5 items-center flex-1 roboto">
+                            <span className="icon" style={{ ['--off' as string]: "url('app-icon/dashboard_icon_solid.svg')" }}></span>
+                            <span>Dashboard</span>
+                        </div>
+                    </a>
+                </li>
+                <li className="overflow-hidden">
+                    <a className={`sidebar-link rounded flex px-4 items-center hover:bg-gray-100 my-1 h-11 text-gray-500 hover:text-gray-600 font-medium`} href="/">
+                        <div className="flex gap-2.5 items-center flex-1 roboto">
+                            <span className="icon" style={{ ['--off' as string]: "url('app-icon/receipt.svg')" }}></span>
+                            <span>Orders List</span>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export const SidebarNavigation = React.memo(SidebarElement)
--- resources/js/components/admin/organisms/sidebar/Sidebar.tsx
+++ resources/js/components/admin/organisms/sidebar/Sidebar.tsx
@@ -1,11 +1,13 @@
 import { SidebarBrand } from '../../atoms'
 import '../../../../../css/sidebar.css'
+import { SidebarNavigation } from '../../molecules'
 
 export const Sidebar: React.FC = () => {
     return (
         <>
             <aside className="sidebar bg-white">
                 <SidebarBrand />
+                <SidebarNavigation />
             </aside>
         </>
     )
