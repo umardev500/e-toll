6189592551105e214f4cf39546327482cc9142a2
umar <umar.dev500@gmail.com>
2023-03-23 07:25:27 +0700

    fix image not loaded

--- resources/js/components/admin/organisms/navbar/RightNavbar.tsx
+++ resources/js/components/admin/organisms/navbar/RightNavbar.tsx
@@ -6,7 +6,7 @@ export const RightNavbar: React.FC = () => {
             <li className="flex relative">
                 <a className="navigation-link avatar-item flex gap-2 items-center px-4 roboto text-gray-500 font-medium" href="#">
                     <span>Jhonson</span>
-                    <img src="avatars/avatar-2.png" className="object-cover w-9 h-9 object-top rounded-full" alt="avatar" />
+                    <img src="/avatars/avatar-2.png" className="object-cover w-9 h-9 object-top rounded-full" alt="avatar" />
                 </a>
             </li>
         </ul>
