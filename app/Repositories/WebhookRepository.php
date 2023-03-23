efcb86632c9fec6c229af324eeede39d736161a4
umar <umar.dev500@gmail.com>
2023-03-23 15:02:08 +0700

    initial order webhook

---
+++ app/Repositories/WebhookRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Order;
+
+class WebhookRepositoy
+{
+    public static function setStatus($orderId, $status, $settlementTime)
+    {
+        // $order = Order::where('order_id')
+    }
+}
--- app/Services/WebhookService.php
+++ app/Services/WebhookService.php
@@ -2,10 +2,16 @@
 
 namespace App\Services;
 
+use Carbon\Carbon;
+
 class WebhookService
 {
     public static function setStatus(array $requestData)
     {
-        return $requestData;
+        $settlementTime = $requestData['settlement_time'];
+        $settlementTimeUnix = Carbon::createFromFormat('Y-m-d H:i:s', $settlementTime, 'Asia/Jakarta')->timestamp;
+        $orderId = $requestData['order_id'];
+        $status = $requestData['transaction_status'];
+        return $status;
     }
 }
