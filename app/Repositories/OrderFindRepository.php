5fc7a27bd5a67cb6c119717684824ffec00c7b1f
umar <umar.dev500@gmail.com>
2023-03-24 17:15:30 +0700

    add search by query params

--- app/Repositories/OrderFindRepository.php
+++ app/Repositories/OrderFindRepository.php
@@ -3,15 +3,28 @@
 namespace App\Repositories;
 
 use App\Models\Order;
+use Illuminate\Support\Facades\Log;
 
 class OrderFindRepository
 {
-    public static function find($perPage, $phoneNumber)
+    public static function find($perPage, $phoneNumber, $search)
     {
         $query = Order::with('productCopy.brand');
         if (!empty($phoneNumber)) {
             $query->where('orders.phone_number', $phoneNumber);
         }
+
+        if (!empty($search)) {
+            $query->orWhere('orders.order_id', 'LIKE', '%' . $search . '%')
+                ->orWhere('orders.phone_number', 'LIKE', '%' . $search . '%')
+                ->orWhere('orders.va', 'LIKE', '%' . $search . '%')
+                ->orWhereHas('productCopy', function ($q) use ($search) {
+                    $q->whereHas('brand', function ($q) use ($search) {
+                        $q->where('name', 'LIKE', '%' . $search . '%');
+                    });
+                });
+        }
+
         $query = $query->orderBy('created_at', 'desc');
         $orders =  $query->simplePaginate(perPage: $perPage);
 
--- app/Services/OrderFindService.php
+++ app/Services/OrderFindService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use App\Models\Order;
 use App\Repositories\OrderFindRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class OrderFindService
 {
@@ -12,9 +13,10 @@ class OrderFindService
     {
         $phoneNumber = $req->input('phone');
         $perPage = $req->input('per_page', '10');
+        $search = $req->query('search');
         $perPage = intval($perPage);
 
-        return OrderFindRepository::find(perPage: $perPage, phoneNumber: $phoneNumber);
+        return OrderFindRepository::find(perPage: $perPage, phoneNumber: $phoneNumber, search: $search);
     }
 
     public static function findOne($id)
