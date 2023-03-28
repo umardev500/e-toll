6a3c023a047388177f14676c1e805418b963a811
umar <umar.dev500@gmail.com>
2023-03-28 16:39:04 +0700

    rename toll to credit

--- resources/js/components/admin/molecules/orderListing/OrderListing.tsx
+++ resources/js/components/admin/molecules/orderListing/OrderListing.tsx
@@ -30,7 +30,7 @@ export const AdminOrderListing: React.FC<Props> = ({ order, index, callback }) =
         <tr>
             <td className="px-4 border-r border-b border-slate-200 py-2 text-center">{index}.</td>
             <td className="px-4 border-r border-b border-slate-200 py-2">{order.order_id}</td>
-            <td className="px-4 border-r border-b border-slate-200 py-2">{toCurrency(product.toll)}</td>
+            <td className="px-4 border-r border-b border-slate-200 py-2">{toCurrency(product.credit)}</td>
             <td className="px-4 border-r border-b border-slate-200 py-2">{toCurrency(product.price, 'Rp')}</td>
             <td className="px-4 border-r border-b border-slate-200 py-2">{phone}</td>
             <td className="px-4 border-r border-b border-slate-200 py-2">{toUpperFirst(getStatus())}</td>
