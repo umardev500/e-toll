bec51e1b143351d6dfbc2a12722b1fb8c9d2475a
umar <umar.dev500@gmail.com>
2023-03-28 19:10:21 +0700

    make brand name unique

--- database/migrations/2023_03_18_034144_brand.php
+++ database/migrations/2023_03_18_034144_brand.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('brand_id');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->json('prefix');
             $table->string('status')->default('active');
             TimeStamps::addTimeStamps($table);
