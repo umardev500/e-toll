be1c811eb34be16b4e3fc40619bcd97c95a92344
umar <umar.dev500@gmail.com>
2023-03-30 20:13:11 +0700

    add link to featured

--- resources/js/components/admin/organisms/featuredList/FeaturedList.tsx
+++ resources/js/components/admin/organisms/featuredList/FeaturedList.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Featured } from '../../molecules'
 
 interface Props {
@@ -11,10 +12,18 @@ interface Props {
 export const FeaturedList: React.FC<Props> = ({ orders, newOrder, pending, confirmation }) => {
     return (
         <div className={`grid lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 pt-5`}>
-            <Featured icon="url('/app-icon/book-filled.svg')" color="#94a3b8" title="Orders" subTitle={orders} />
-            <Featured icon="url('/app-icon/task.svg')" color="#84cc16" title="New Orders" subTitle={newOrder} />
-            <Featured icon="url('/app-icon/receipt-filled.svg')" color="#eab308" title="Orders Pending" subTitle={pending} />
-            <Featured icon="url('/app-icon/task-featured.svg')" color="#16a34a" title="Orders Confirmation" subTitle={confirmation} />
+            <Link className="cursor-pointer" to="/admin/orders">
+                <Featured icon="url('/app-icon/book-filled.svg')" color="#94a3b8" title="Orders" subTitle={orders} />
+            </Link>
+            <Link className="cursor-pointer" to="/admin/orders?sort=desc">
+                <Featured icon="url('/app-icon/task.svg')" color="#84cc16" title="New Orders" subTitle={newOrder} />
+            </Link>
+            <Link className="cursor-pointer" to="/admin/orders?sort=desc&status=pending">
+                <Featured icon="url('/app-icon/receipt-filled.svg')" color="#eab308" title="Orders Pending" subTitle={pending} />
+            </Link>
+            <Link className="cursor-pointer" to="/admin/orders?sort=desc&status=settlement">
+                <Featured icon="url('/app-icon/task-featured.svg')" color="#16a34a" title="Orders Confirmation" subTitle={confirmation} />
+            </Link>
         </div>
     )
 }
