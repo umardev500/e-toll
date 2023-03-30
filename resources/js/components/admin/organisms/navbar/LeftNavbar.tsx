a54a7c14dce20059dfcb79f183922eabfe55cb43
umar <umar.dev500@gmail.com>
2023-03-30 23:16:55 +0700

    home link to home

--- resources/js/components/admin/organisms/navbar/LeftNavbar.tsx
+++ resources/js/components/admin/organisms/navbar/LeftNavbar.tsx
@@ -4,7 +4,7 @@ export const LeftNavbar: React.FC = () => {
     return (
         <ul className="navigation flex">
             <li>
-                <a href={'/'} className="navigation-link px-4 roboto text-gray-500 hover:text-gray-400 font-medium">
+                <a href={'/admin'} className="navigation-link px-4 roboto text-gray-500 hover:text-gray-400 font-medium">
                     Home
                 </a>
             </li>
