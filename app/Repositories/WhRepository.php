bb32708b0a2e900db2aee2ee7496e984eedb6293
umar <umar.dev500@gmail.com>
2023-03-31 12:56:03 +0700

    add set expired_at on create order

--- app/Repositories/WhRepository.php
+++ app/Repositories/WhRepository.php
@@ -8,12 +8,15 @@ use Illuminate\Support\Facades\Log;
 
 class WhRepository
 {
-    public static function setStatus($orderId, $status, $settlementTime)
+    public static function setStatus($orderId, $status, $settlementTime, $expiry)
     {
         $updatedTime = Carbon::now()->timestamp;
         $payload = ['status' => $status, 'updated_at' => $updatedTime];
         if ($status === 'settlement') {
             $payload['settlement_time'] = $settlementTime;
+        }
+        if ($expiry != null) {
+            $payload['expired_at'] = $expiry;
         }
 
         $order = Order::where('order_id', $orderId)
--- app/Services/WebhookService.php
+++ app/Services/WebhookService.php
@@ -13,12 +13,19 @@ class WebhookService
     {
         $status = $requestData['transaction_status'];
         $settlementTimeUnix = null;
+        $expiryUnix = null;
         if ($status == 'settlement') {
             $settlementTime = $requestData['settlement_time'];
             $settlementTimeUnix = Carbon::createFromFormat('Y-m-d H:i:s', $settlementTime, 'Asia/Jakarta')->timestamp;
         }
-        $orderId = $requestData['order_id'];
+        if ($status == 'pending') {
+            $expiryTime = $requestData['expiry_time'];
+            $expiryUnix = Carbon::createFromFormat('Y-m-d H:i:s', $expiryTime, 'Asia/Jakarta')->timestamp;
+        }
 
-        return WhRepository::setStatus($orderId, $status, $settlementTimeUnix);
+        $orderId = $requestData['order_id'];
+        Log::info($requestData);
+
+        return WhRepository::setStatus($orderId, $status, $settlementTimeUnix, $expiryUnix);
     }
 }
