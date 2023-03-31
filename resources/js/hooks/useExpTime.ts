4c347bd1d78d434821d752b805eb6e4ee15409c9
umar <umar.dev500@gmail.com>
2023-03-31 13:38:57 +0700

    fix status not update on reload

--- resources/js/hooks/useExpTime.ts
+++ resources/js/hooks/useExpTime.ts
@@ -1,26 +1,24 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { getServerTime } from '../helpers'
 
 export const useExpTime = (from: number): boolean => {
     const [isExp, setIsExp] = useState<boolean>(false)
 
-    useEffect(() => {
-        const calculate = async (): Promise<void> => {
-            try {
-                const unixTime = await getServerTime()
-                if (from > 0) setIsExp(unixTime > from)
+    const calculate = async (): Promise<void> => {
+        try {
+            const unixTime = await getServerTime()
+            if (from > 0) setIsExp(unixTime > from)
 
-                await Promise.resolve()
-                return
-            } catch (err) {
-                await Promise.reject(err)
-            }
+            await Promise.resolve()
+            return
+        } catch (err) {
+            await Promise.reject(err)
         }
+    }
 
-        calculate().catch((err) => {
-            console.log(err)
-        })
-    }, [])
+    calculate().catch((err) => {
+        console.log(err)
+    })
 
     return isExp
 }
